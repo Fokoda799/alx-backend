837faae30f0f1991bae5c038e716762011d0491d
feyt2003 <feyt2003@gmail.com>
2024-07-21 14:22:39 +0000

    ok

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Simple helper function """
+
+
+def index_range(page, page_size) -> tuple[int, int]:
+    """
+    Return a
+    tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Args:
+        page (int): Page number (1-indexed).
+        page_size (int): Number of items per page.
+
+
+    Returns:
+        Tuple[int, int]: Tuple containing start and end indexes.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
