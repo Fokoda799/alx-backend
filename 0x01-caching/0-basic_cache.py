4ee0cdb32a66a9c72042a9954e215b9bd9541e89
feyt2003 <feyt2003@gmail.com>
2024-07-23 17:47:13 +0000

    ok

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Basic caching class """
+
+
+class BasicCache(BaseCaching):
+    """ Basic caching class """
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the key key.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key.
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
